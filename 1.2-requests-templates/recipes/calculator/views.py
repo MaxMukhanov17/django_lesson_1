f70d2e0fa3e38368ac38c5466a016d475136e2f9
Maxim Mukhanov <momcmop@icloud.com>
2022-05-18 23:38:44 +0300

    First commit

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+
 DATA = {
     'omlet': {
         'яйца, шт': 2,
@@ -28,3 +29,37 @@ DATA = {
 #     'ингредиент2': количество2,
 #   }
 # }
+def omlet_view(request, servings):
+    servings = int(request.GET.get('servings', 1))
+    context = {
+        'recipe': {
+            'яйца, шт': 2*servings,
+            'молоко, л': 0.1*servings,
+            'соль, ч.л.': 0.5*servings,
+        }
+    }
+    return render(request, 'calculator/index.html', context)
+    
+
+def pasta_view(request, servings):
+    servings = int(request.GET.get('servings', 1))
+    context = {
+        'recipe': {
+            'макароны, г': 0.3*servings,
+            'сыр, г': 0.05*servings,
+        }
+    }
+    return render(request, 'calculator/index.html', context)
+
+
+def buter_view(request, servings):
+    servings = int(request.GET.get('servings', 1))
+    context = {
+        'recipe': {
+            'хлеб, ломтик': 1*servings,
+            'колбаса, ломтик': 1*servings,
+            'сыр, ломтик': 1*servings,
+            'помидор, ломтик': 1*servings,
+        }
+    }
+    return render(request, 'calculator/index.html', context)
